cc34edc5b647cbb51da204a30ba44db9ed3a8602
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-01-13 03:30:42 +0900

    fix: add xml comment and minor fix

--- src/CenterFaceDotNet/NonMaximumSuppressionMode.cs
+++ src/CenterFaceDotNet/NonMaximumSuppressionMode.cs
@@ -1,11 +1,20 @@
 ﻿namespace CenterFaceDotNet
 {
 
+    /// <summary>
+    /// Specifies the mode of Non Maximum Suppression.
+    /// </summary>
     public enum NonMaximumSuppressionMode
     {
 
+        /// <summary>
+        /// Specifies that the union nms.
+        /// </summary>
         Union = 1,
 
+        /// <summary>
+        /// Specifies that the minimum nms.
+        /// </summary>
         Minimum = 2
 
     }
