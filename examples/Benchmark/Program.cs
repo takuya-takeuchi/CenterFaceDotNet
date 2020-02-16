dc573ab33443ba1d98f52111f12f6e29993106f9
Takuya Takeuchi <takuya.takeuchi.dev@gmail.com>
2020-02-16 16:19:22 +0900

    feat: add Benchmark program

---
+++ examples/Benchmark/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using NcnnDotNet.OpenCV;
+using ShellProgressBar;
+using CenterFaceDotNet;
+
+namespace Benchmark
+{
+
+    internal class Program
+    {
+
+        #region Methods
+
+        private static int Main(string[] args)
+        {
+            if (args.Length <= 3)
+            {
+                Console.WriteLine($"Usage: {nameof(Benchmark)} < ncnn bin > < ncnn param >  < image files > < loop count >");
+                return 1;
+            }
+
+            var binPath = args[0];
+            var paramPath = args[1];
+            var imagePath = args[2];
+            var maxLoop = int.TryParse(args[3], out var ret) ? ret : 1000;
+            
+            var param = new CenterFaceParameter
+            {
+                BinFilePath = binPath,
+                ParamFilePath = paramPath
+            };
+
+            using (var centerFace = CenterFace.Create(param))
+            {
+                Console.WriteLine($"Processing {imagePath}");
+
+                using var frame = Cv2.ImRead(imagePath);
+
+                var imageStopWatch = new Stopwatch();
+                var detectStopWatch = new Stopwatch();
+
+                var totalImageLoad = 0d;
+                var totalDetect = 0d;
+
+                var options = new ProgressBarOptions
+                {
+                    ProgressCharacter = '─',
+                    ForegroundColor = ConsoleColor.Yellow,
+                    ForegroundColorDone = ConsoleColor.DarkGreen,
+                    BackgroundColor = ConsoleColor.DarkGray,
+                    ProgressBarOnBottom = true,
+                };
+
+                using (var pbar = new ProgressBar(maxLoop, "Initial message", options))
+                    for (var loop = 1; loop <= maxLoop; loop++)
+                    {
+                        imageStopWatch.Start();
+                        using var inMat = NcnnDotNet.Mat.FromPixels(frame.Data, NcnnDotNet.PixelType.Bgr2Rgb, frame.Cols, frame.Rows);
+                        imageStopWatch.Stop();
+                        totalImageLoad += imageStopWatch.ElapsedMilliseconds;
+
+                        detectStopWatch.Start();
+                        var _ = centerFace.Detect(inMat, frame.Cols, frame.Rows).ToArray();
+                        detectStopWatch.Stop();
+                        totalDetect += detectStopWatch.ElapsedMilliseconds;
+
+                        pbar.Tick($"Step {loop} of {maxLoop}");
+                    }
+
+                Console.WriteLine($"Total Loop {maxLoop}");
+                Console.WriteLine($"\tConvert Image to Mat: Total {totalImageLoad} ms, Avg {totalImageLoad / maxLoop} ms");
+                Console.WriteLine($"\t         Detect Face: Total {totalDetect} ms, Avg {totalDetect / maxLoop} ms");
+                Console.WriteLine($"\t    Total Throughput: Total {totalImageLoad + totalDetect} ms, Avg {(totalImageLoad + totalDetect) / maxLoop} ms");
+            }
+
+            return 0;
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ examples/Benchmark/README.md
@@ -0,0 +1,39 @@
+﻿# Benchmark
+
+This example demonstrates performance of detecting faces from images file.
+
+## How to use?
+
+## 1. Preparation
+
+This example requires test images, model adn parameter files. 
+Download test data from the following urls.
+
+- https://github.com/Star-Clouds/CenterFace/tree/master/models/ncnn
+  - centerface.bin
+  - centerface.param
+
+And copy to these files to &lt;Demo_dir&gt;.
+
+## 2. Build
+
+1. Open command prompt and change to &lt;Demo_dir&gt;
+1. Type the following command
+````
+dotnet build -c Release
+````
+
+## 3. Run
+
+1. Open command prompt and change to &lt;Demo_dir&gt;
+1. Type the following sample command
+````
+$ dotnet run -c Release -- centerface.bin centerface.param test.jpg 100
+
+Processing test.jpg
+100.00% Step 100 of 100                                                                                                                        00:01:24───────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+Total Loop 100
+        Convert Image to Mat: Total 30998 ms, Avg 309.98 ms
+                 Detect Face: Total 4176183 ms, Avg 41761.83 ms
+            Total Throughput: Total 4207181 ms, Avg 42071.81 ms
+````
